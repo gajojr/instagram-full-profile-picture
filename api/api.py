8567a4f89e6d9cba710af7dab108cdbe88726ca6
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-07-22 21:58:16 +0200

    refactor: switch outside api for fetching profile picture

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
 import os
+import shutil
 from flask import Flask, Response, request, send_file
-from ig_profile import ProfileImage as pimage
+# from ig_profile import ProfileImage as pimage
+import instaloader
 
 app = Flask(__name__)
 
@@ -8,12 +10,14 @@ app = Flask(__name__)
 def get_profile_picture():
     try:
         username = request.args.get('username')
-        pimage.image(username)
-        return send_file(f'{username}.png')
+        mod = instaloader.Instaloader()
+        mod.download_profile(username, profile_pic_only=True)
+        # pimage.image(username)
+        return send_file(f'{username}/' + os.listdir(f'{username}')[0])
     except IndexError:
         r = Response(response="Outside API error occurred", status=500, mimetype="application/xml")
         r.headers["Content-Type"] = "text/xml; charset=utf-8"
         return "Outside API error occurred"
     finally:
-        if os.path.isfile(f'./{username}.png'):
-            os.remove(f'{username}.png')
+        if os.path.isdir(username):
+            shutil.rmtree(username)
